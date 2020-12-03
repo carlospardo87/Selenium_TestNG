72179e13f838ad63bc93271830fa1b5f8f80c93a
Carlos Pardo <carlos@MacBook-Air.local>
2020-12-03 09:52:07 -0300

    Created test PurchaseFlowAlexa

---
+++ src/test/java/PurchaseFlowAlexaTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class PurchaseFlowAlexaTest {
+
+
+    @Test(description = "Purchase Flow Alexa in Amazon")
+    public void AlexaFlow(){
+
+        System.out.println("Given a user navigates to www.amazon.com");
+        System.out.println("And search for Alexa");
+        System.out.println("And click on button search");
+        System.out.println("And navigate to the second page");
+        System.out.println("And select the third item");
+        System.out.println("And clicks on button Add to Cart");
+        System.out.println("Then checks product is display properly");
+
+    }
+}
