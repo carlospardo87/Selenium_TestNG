000be7c4136b2b251bdcef2d143a089084c7427c
Carlos Pardo <carlos@MacBook-Air.local>
2020-12-03 14:47:51 -0300

    Added methods to check if the buttons are visible

--- src/main/java/pages/ProcessPaymentPage.java
+++ src/main/java/pages/ProcessPaymentPage.java
@@ -8,8 +8,9 @@ public class ProcessPaymentPage extends BasePage {
 
     public WebDriver driver;
 
-    private final By BUTTON_PROCESS = By.id("hlb-ptc-btn-native");
+    private final By BUTTON_CHECKOUT = By.id("hlb-ptc-btn-native");
     private final By CART_ITEM = By.id("nav-cart-count");
+    private final By BUTTON_CART = By.id("hlb-view-cart-announce");
 
 
 
@@ -18,11 +19,15 @@ public class ProcessPaymentPage extends BasePage {
         this.driver = driver;
     }
 
-    public String getTextButtonProcess(){
-        return getText(BUTTON_PROCESS);
-    }
-
     public String getCartItems(){
         return getText(CART_ITEM);
+    }
+
+    public Boolean getButtonCheckoutStatus() {
+        return isDisplayed(BUTTON_CHECKOUT);
+    }
+
+    public Boolean getButtonCartStatus() {
+        return isDisplayed(BUTTON_CART);
     }
 }
